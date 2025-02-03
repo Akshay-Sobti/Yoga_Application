57bd4059f151547fbb6f08f41fec9390f873922f
Akshay-Sobti <akshaysobti2002@gmail.com>
2025-02-03 17:26:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-nam
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Use Local MongoDB URI for Compass
+const uri = "mongodb://127.0.0.1:27017/yoga_database"; // Local MongoDB
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+const usersRouter = require("./routes/users.js");
+app.use("/", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
